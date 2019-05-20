b52b3537dd282a86cb5b9ea8d407ebd7aa17edc2
Kyle Simpson <kyle3381@gmail.com>
2019-05-20 15:26:41 -0700

    Add to plots

--- kyle_analysis/analysis.R
+++ kyle_analysis/analysis.R
@@ -7,31 +7,37 @@ state <- state[1:51,c("State", "WIC.participants.FY.2009", "WIC.participants.FY.
                   "WIC.participants..FY.2012", "WIC.participants..FY.2013", "WIC.participants..FY.2014",
                   "WIC.participants..FY.2015")]
 colnames(state) <- c("state", "WIC2009", "WIC2011", "WIC2012", "WIC2013", "WIC2014", "WIC2015")
-View(state)
+# View(state)
 
 
 wic09_plot <- plot_usmap(data=state, values="WIC2009", lines="white") +
-  scale_fill_continuous(low="white", high="blue", name = "WIC Participants 2009", label = scales::comma) +
+  labs(title="WIC Participants 2009") +
+  scale_fill_continuous(low="white", high="blue", name = "Num. Participants", label = scales::comma) +
   theme(legend.position = "right")
 
 wic11_plot <- plot_usmap(data=state, values="WIC2011", lines="white") +
-  scale_fill_continuous(low="white", high="blue", name = "WIC Participants 2011", label = scales::comma) +
+  labs(title="WIC Participants 2011") +
+  scale_fill_continuous(low="white", high="blue", name = "Num. Participants", label = scales::comma) +
   theme(legend.position = "right")
 
 wic12_plot <- plot_usmap(data=state, values="WIC2012", lines="white") +
-  scale_fill_continuous(low="white", high="blue", name = "WIC Participants 2012", label = scales::comma) +
+  labs(title="WIC Participants 2012") +
+  scale_fill_continuous(low="white", high="blue", name = "Num. Participants", label = scales::comma) +
   theme(legend.position = "right")
 
 wic13_plot <- plot_usmap(data=state, values="WIC2013", lines="white") +
-  scale_fill_continuous(low="white", high="blue", name = "WIC Participants 2013", label = scales::comma) +
+  labs(title="WIC Participants 2013") +
+  scale_fill_continuous(low="white", high="blue", name = "Num. Participants", label = scales::comma) +
   theme(legend.position = "right")
 
 wic14_plot <- plot_usmap(data=state, values="WIC2014", lines="white") +
-  scale_fill_continuous(low="white", high="blue", name = "WIC Participants 2014", label = scales::comma) +
+  labs(title="WIC Participants 2014") +
+  scale_fill_continuous(low="white", high="blue", name = "Num. Participants", label = scales::comma) +
   theme(legend.position = "right")
 
 wic15_plot <- plot_usmap(data=state, values="WIC2015", lines="white") +
-  scale_fill_continuous(low="white", high="blue", name = "WIC Participants 2015", label = scales::comma) +
+  labs(title="WIC Participants 2015") +
+  scale_fill_continuous(low="white", high="blue", name = "Num. Participants", label = scales::comma) +
   theme(legend.position = "right")
 
 

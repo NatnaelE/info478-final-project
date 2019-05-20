94a56e0823fd76df6fbe113dec9751829d3efb5b
Kyle Simpson <kyle3381@gmail.com>
2019-05-20 14:47:42 -0700

    Work on data prep.  Begin analysis

--- data_prep.R
+++ data_prep.R
@@ -1,8 +1,25 @@
 library(dplyr)
-install.packages("readxl")
 library(readxl)
+
 # xls files
-pop_estimates <- read_excel("./data/raw/DataDownload.xls", sheet = 3) # Sheets 3 - 14
+county_estimates <- read_excel("./data/raw/DataDownload.xls", sheet = 3)
+state_estimates <- read_excel("./data/raw/DataDownload.xls", sheet = 4)
+access <- read_excel("./data/raw/DataDownload.xls", sheet = 5)
+stores <- read_excel("./data/raw/DataDownload.xls", sheet = 6)
+restaurants <- read_excel("./data/raw/DataDownload.xls", sheet = 7)
+assistance <- read_excel("./data/raw/DataDownload.xls", sheet = 8)
+insecurity <- read_excel("./data/raw/DataDownload.xls", sheet = 9)
+prices_taxes <- read_excel("./data/raw/DataDownload.xls", sheet = 10)
+local <- read_excel("./data/raw/DataDownload.xls", sheet = 11)
+health <- read_excel("./data/raw/DataDownload.xls", sheet = 12)
+socioeconomic <- read_excel("./data/raw/DataDownload.xls", sheet = 13)
+
+# Manipulate individual dataframes as necessary (remove as many unneeded columns as possible)
+# Documentation: https://www.ers.usda.gov/data-products/food-environment-atlas/documentation/#health
 
 
 
+
+
+# Example csv output:
+# write.csv(state_estimates, "./data/prepped/state_estimates.csv")
